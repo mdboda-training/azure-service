e4997d1c055619928f5711316354405123aace5b
unknown <boda.gadhilinga@gmail.com>
2025-02-20 16:38:21 +0530

    Changes

--- src/main/java/com/mdsolutions/azure/service/HomeController.java
+++ src/main/java/com/mdsolutions/azure/service/HomeController.java
@@ -1,3 +1,5 @@
+package com.mdsolutions.azure.service;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
